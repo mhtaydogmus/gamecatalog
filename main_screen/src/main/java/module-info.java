e026adcaf48ab614f17789b8f86529fd7b6b034e
LordVader104 <120245095+LordVader104@users.noreply.github.com>
2025-04-05 14:23:48 +0300

    Delete main_screen directory

--- main_screen/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.main_screen {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.main_screen to javafx.fxml;
-    exports com.example.main_screen;
-}
